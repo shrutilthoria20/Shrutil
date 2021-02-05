90f00ae8b926aa42d4aa39dd8d84833d6ee00bae
shrutilthoria20 <78613849+shrutilthoria20@users.noreply.github.com>
2021-02-05 23:49:05 +0530

    Add files via upload

---
+++ IP_Project/1st.js
@@ -0,0 +1,23 @@
+
+		function validate_form() 
+		{
+			if(document.form.email.value=="")
+			{
+				alert("Enter your Username");
+				return false;
+			}
+			if(document.form.password.value=="")
+			{
+				alert("Enter password");
+				return false;
+			}
+
+
+        }
+        
+
+function xyz()
+{
+	confirm("Confirm your Order");
+	window.open("thanks(2).html");
+}		
